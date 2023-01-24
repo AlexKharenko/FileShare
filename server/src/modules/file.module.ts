8831ee1eaf52eb2ba10d937d859f4a34a8a12d31
AlexKharenko <alexey16052002@gmail.com>
2023-01-24 22:48:07 +0200

    Added module

--- server/src/modules/file.module.ts
+++ server/src/modules/file.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FileController } from 'src/controllers/file.controller';
 import { FileService } from 'src/services/file.service';
 import { DbModule } from './db.module';
+import { FolderModule } from './folder.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, FolderModule],
   controllers: [FileController],
   providers: [FileService],
   exports: [FileService],
