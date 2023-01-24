73d207b4cc68899dc45371e469d5a3e7d0771ef1
AlexKharenko <alexey16052002@gmail.com>
2023-01-24 22:46:18 +0200

    Added cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+  app.enableCors({
+    origin: ['http://localhost:8080', 'http://192.168.1.6:8080'],
+    credentials: true,
+  });
   await app.register(cookie);
   await app.listen(3000, '0.0.0.0');
 }
